2da6817d4168414977bb14538c37f55f573f3861
baoduong1011 <baoduong101110@gmail.com>
2022-06-07 08:39:41 +0700

    CI_Workflows

--- utils/random.go
+++ utils/random.go
@@ -12,17 +12,16 @@ func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
-
 // RandomInt generates a random integer between min and max
-func RandomInt(min,max int64) int64 {
-	return min+rand.Int63n(max-min+1) // return value 0 -> max-min because Int63n return a random integer between 0 and n-1 -> so if you plus min -> [min,max]
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1) // return value 0 -> max-min because Int63n return a random integer between 0 and n-1 -> so if you plus min -> [min,max]
 }
 
-//RandomString generates a random string of length n 
+//RandomString generates a random string of length n
 func RandomString(n int) string {
 	var sb strings.Builder
 	k := len(alphabet)
-	for i := 0;i<n;i++ {
+	for i := 0; i < n; i++ {
 		c := alphabet[rand.Intn(k)]
 		sb.WriteByte(c)
 	}
@@ -36,11 +35,11 @@ func RandomOwner() string {
 }
 
 func RandomMoney() int64 {
-	return RandomInt(0,1000)
+	return RandomInt(0, 1000)
 }
 
 func RandomCurrency() string {
-	currencies := []string{"EUR","USD","CAD"}
+	currencies := []string{"EUR", "USD", "CAD"}
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
 }
@@ -48,12 +47,12 @@ func RandomCurrency() string {
 // --- RANDOM TRANSFERS ---
 
 func RandomAmout() int64 {
-	return RandomInt(0,20)
+	return RandomInt(0, 20)
 }
 
 func RandomFromID() int64 {
-	return RandomInt(0,40)
+	return RandomInt(0, 40)
 }
 func RandomToID() int64 {
-	return RandomInt(40,80)
+	return RandomInt(40, 80)
 }
